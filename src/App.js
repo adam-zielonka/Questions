66ecf878b5930477b02670c2d9d13ba9978f8d30
Adam Zielonka <root@adamzielonka.pro>
2019-06-30 08:04:43 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { observer } from 'mobx-react-lite'
+
+function App() {
+
+  return (
+    <div className="App">
+      Parser
+    </div>
+  )
+}
+
+export default observer(App)
