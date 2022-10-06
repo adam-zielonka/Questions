3a000906f09e5a5a28fbcb9bf884a035cfc8b8a6
Adam Zielonka <root@adamzielonka.pro>
2022-10-06 21:01:18 +0200

    Update urls to repo

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ import Card from './Card'
 function About() {
   return <Card>
     <a href='https://adamzielonka.pro/'>adamzielonka.pro</a>
-    , <a href='https://gitlab.com/adam-zielonka-pro/questions'>source code</a>
-    , <a href='https://gitlab.com/adam-zielonka-pro/questions/blob/master/src/assets/questions.json'>quiz sample</a>
+    , <a href='https://github.com/adam-zielonka/questions'>source code</a>
+    , <a href='https://github.com/adam-zielonka/questions/blob/main/src/assets/questions.json'>quiz sample</a>
   </Card>
 }
 
